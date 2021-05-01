d5c851d4faf25e8e5f218a1d8e26628c90f4e2d7
n-fukuju <n.fukuju@gmail.com>
2021-05-01 14:57:47 +0900

    リファクタリング
    不要なコメントアウトを削除

--- README.md
+++ README.md
@@ -3,32 +3,39 @@ Plotexr
 
 本拡張は、想定通りの物書きができているか検証するためのツリービューを提供します。  
 主に小説用を想定しています。  
+
 ![file](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature.png)  
 
 
 ## クイックスタート
 * エクスプローラから、「ファイルを登録」します。  
+
 ![file1](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-file1.png)  
 
 * 追加されたファイルのメニューから、「項目を追加」します。  
-* 追加する項目は、今回の例では「最小」です。  
+  * 追加する項目は、今回の例では「最小」です。  
+
 ![file2](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-file2.png)  
   
 ![file3](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-file3.png)  
 
 * ファイルに対する検証項目として、最低ラインが登録されます。  
-* 薄字で、現在のファイル内容が表示されます。  
+  * 薄字で、現在のファイル内容が表示されます。  
+
 ![file4](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-file4.png)  
 
 * 検証値を変えるため、メニューから「更新」します。  
-* 今回は、25枚（400字詰め換算）とします。  
+  * 今回は、25枚（400字詰め換算）とします。  
+
 ![file5](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-file5.png)  
   
 ![file6](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-file6.png)  
 * ファイル内容が不足するため、チェックマークが消えます。  
+
 ![file7](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-file7.png)  
 
-* ファイル内容を更新して再度検証する場合、ツリービューの「更新」を実行します。
+* ファイル内容を更新して再度検証する場合、ツリービューの「更新」を実行します。  
+
 ![file8](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-file8.png)  
 
 
@@ -42,7 +49,8 @@ Plotexr
 なお、記述項目は行単位で検証しています。行をまたいだ検証は行いません。  
 
 ## 設定ファイル
-* plot.json に設定が保存されます。複数人での作業に本拡張を用いる場合、設定ファイルも併せて共有してください。
+* plot.json に設定が保存されます。複数人での作業に本拡張を用いる場合、設定ファイルも併せて共有してください。  
+
 ![json](https://github.com/n-fukuju/plot-verifier-vsce/raw/master/images/feature-json.png)   
 * 拡張機能アンインストール時に設定ファイルの削除は行われないため、不要な場合、手作業で削除してください。
 * plot.json を直接編集した場合は、ツリービューの「更新」を実行することで、メモリ上の設定を破棄して再読み込みが行われます。
--- src/plotVerifyProvider.ts
+++ src/plotVerifyProvider.ts
@@ -26,14 +26,8 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         // });
         // 常時表示
         vscode.commands.executeCommand('setContext', 'plotVerifierEnabled', true);
-
-        // // プロットを読み込み
-        // this.getPlot(true);
         // 初回
         this.refresh();
-        
-        // 初回実行
-        // this.onActiveEditorChanged();
 
     }
 
@@ -52,18 +46,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
             {
                 const chapter:Chapter = element.value as Chapter;
                 return Promise.resolve(chapter.properties);
-                // const properties:Element[] = [];
-                // // 子要素をセット
-                // properties.push(new Element(ElementType.FILE, chapter.file));
-                // // properties.push(new Element(ElementType.ALERT, chapter.alert));
-                // properties.push(new Element(ElementType.DEADLINE, chapter.deadline));
-                // properties.push(new Element(ElementType.MAXIMUM, chapter.maximum));
-                // properties.push(new Element(ElementType.MINIMUM, chapter.minimum));
-                // for(let condition of chapter.conditions)
-                // {
-                //     properties.push(new Element(ElementType.CONDITION, condition));
-                // }
-                // return Promise.resolve(properties);
             }
             else
             {
@@ -78,13 +60,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
 
             // ルート（引数が空）の場合、Chapter 一覧を返す
             return Promise.resolve(this.plot.chapterElements);
-            // const chapters:Element[] = [];
-            // for(const c of this.plot.chapters)
-            // {
-            //     const e = new Element(ElementType.CHAPETER, c,);
-            //     chapters.push(e);
-            // }
-            // return Promise.resolve(chapters);
         }
     }
 
@@ -96,11 +71,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         {
             case ElementType.chapter:
                 const chapter:Chapter = element.value as Chapter;
-                // let chapterItem = new vscode.TreeItem(chapter.file, vscode.TreeItemCollapsibleState.Expanded);
-                // item.command = {};
-                // chapterItem.iconPath = this.getIcon(Icon.CHECK);
-                // chapterItem.tooltip = "tooltip";
-                // chapterItem.description = "description";
                 const chapterItem = new vscode.TreeItem(chapter.fileElement.label, vscode.TreeItemCollapsibleState.Expanded);
                 chapterItem.description = chapter.fileElement.description;
                 // 選択アイコン表示用の値。（package.json の、view/item/context）
@@ -111,18 +81,7 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
                     chapterItem.collapsibleState = vscode.TreeItemCollapsibleState.None;
                     chapterItem.iconPath = this.getIcon(Icon.alert);
                 }
-                // console.log('chapterItem: ', chapterItem);
                 return chapterItem;
-        // }
-        // else if(element.type == ElementType.FILE)
-        // {
-            // case ElementType.FILE:
-            //     let fileItem = new vscode.TreeItem(element.value, vscode.TreeItemCollapsibleState.None);
-            //     return fileItem;
-            // case ElementType.ALERT:
-            //     let alertItem = new vscode.TreeItem(element.value, vscode.TreeItemCollapsibleState.None);
-            //     alertItem.iconPath = this.getIcon(Icon.CALENDER);
-            //     return alertItem;
             case ElementType.deadline:
                 if(!element.value || element.value === ""){ return null; }
                 let deadItem = new vscode.TreeItem(element.value, vscode.TreeItemCollapsibleState.None);
@@ -134,9 +93,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
             case ElementType.minimum:
                 if(!element.value || element.value === ""){ return null; }
                 let miniItem = new vscode.TreeItem(element.value, vscode.TreeItemCollapsibleState.None);
-                // miniItem.label = `最低 ${element.value}字`
-                // miniItem.iconPath = this.getIcon(Icon.PENCIL)
-                // miniItem.description = "残りn字"
                 miniItem.contextValue = 'minimum';
                 miniItem.label = element.label;
                 miniItem.description = element.description;
@@ -145,9 +101,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
             case ElementType.maximum:
                 if(!element.value || element.value === ""){ return null; }
                 let maxItem = new vscode.TreeItem(element.value, vscode.TreeItemCollapsibleState.None);
-                // maxItem.label = `最大 ${element.value}字`;
-                // maxItem.iconPath = this.getIcon(Icon.PENCIL);
-                // maxItem.description = "残りn字"
                 maxItem.contextValue = 'maximum';
                 maxItem.label = element.label;
                 maxItem.description = element.description;
@@ -155,7 +108,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
                 return maxItem;
             case ElementType.condition:
                 let conItem = new vscode.TreeItem(element.value, vscode.TreeItemCollapsibleState.None);
-                // conItem.iconPath = this.getIcon(Icon.SEARCH);
                 conItem.contextValue = 'condition';
                 conItem.label = element.label;
                 conItem.description = element.description;
@@ -265,7 +217,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         vscode.window.showQuickPick(['期日', '最小', '最大', '記述'], {placeHolder:'追加する項目'}).then(value=>{
             if(value)
             {
-                // vscode.window.showInformationMessage(value);
                 const chapter:Chapter = element.value as Chapter;
                 switch(value)
                 {
@@ -372,8 +323,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         for(const chapter of this.plot.chapters)
         {
             const filePath = vscode.Uri.joinPath(this.plot.root.uri, chapter.fileElement.value);
-            // console.log(filePath);
-            // console.log(filePath.fsPath);
             // ファイル関連
             if(!fs.existsSync(filePath.fsPath))
             {
@@ -414,17 +363,11 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         if(match)
         {
             chapter.deadlineElement.label = match[0];
-            // description に末尾を記述する処理
-            // chapter.deadlineElement.description = 
-            //     (chapter.deadline.length > chapter.deadlineElement.label.length)
-            //     ? chapter.deadline.substring(chapter.deadlineElement.label.length)
-            //     : chapter.deadline;
 
             // Date コンストラクタ引数の Month は 0起点 なので注意
             const now = new Date();
             const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
             const d = new Date(Number(match[1]), Number(match[2])-1, Number(match[3]));
-            // console.log('match: ', match);
             console.log(`deadline: ${chapter.deadlineElement.value}, date: ${d.toLocaleString('ja')}, today: ${today.toLocaleString('ja')}`);
             if(today.getTime() >= d.getTime())
             {
@@ -439,10 +382,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
             if(match)
             {
                 chapter.deadlineElement.label = match[0];
-                // chapter.deadlineElement.description = 
-                //     (chapter.deadline.length > chapter.deadlineElement.label.length)
-                //     ? chapter.deadline.substring(chapter.deadlineElement.label.length)
-                //     : chapter.deadline;
                 const now = new Date();
                 const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
                 const d = new Date(today.getFullYear(), Number(match[1])-1, Number(match[2]));
@@ -466,8 +405,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
     /** ファイルサイズの確認を行う */
     private compareFileSize(chapter:Chapter, fsPath:string)
     {
-        // this.compareFileSizeRecurse(chapter, true, fsPath);
-        // this.compareFileSizeRecurse(chapter, false, fsPath);
         for(let element of chapter.maximumElements){ this.compareFileSizeRecurse(chapter, fsPath,  true, element); }
         for(let element of chapter.minimumElements){ this.compareFileSizeRecurse(chapter, fsPath, false, element); }
     }
@@ -475,18 +412,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
     /** ファイルサイズの確認を行う */
     private async compareFileSizeRecurse(chapter:Chapter, fsPath:string, max:boolean, element:Element)
     {
-        // 指定されている場合のみ処理
-        // if(!(max && chapter.maximum) && !(!max && chapter.minimum)){ return; }
-
-        // const filePath = vscode.Uri.joinPath(this.plot.root.uri, chapter.file);
-        // const value = (max)? chapter.maximum: chapter.minimum;
-        // const element = (max)? chapter.maximumElement: chapter.minimumElement;
-        
-        // let matchKB = value.match(/^(\d+)KB$/);
-        // let matchMB = value.match(/^(\d+)MB$/);
-        // let matchPage = value.match(/^(\d+)枚$/);
-        // let matchChars = value.match(/^(\d+)字$/);
-        // let matchCount = value.match(/^(\d+)$/);
         let matchKB = element.value.match(/^(\d+)KB$/);
         let matchMB = element.value.match(/^(\d+)MB$/);
         let matchPage = element.value.match(/^(\d+)枚$/);
@@ -495,9 +420,7 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         // 末尾が KB
         if(matchKB) {
             let size = Number(matchKB[1]);
-            // const stat = fs.statSync(filePath.fsPath);
             const stat = fs.statSync(fsPath);
-            // console.log(`size: ${value}, fssize: ${stat.size}`);
             const current = stat.size/1024;
             if(max){
                 element.label = `最大 ${matchKB[0]}`;
@@ -554,15 +477,8 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
             let textCount = 0;
             let pageCount = 0;
             let rowCount = 0;
-            // let percentage = 0;
-            // let percentageStr = "";
             const col = 20;
             const row = 20;
-            // const text = fs.readFileSync(fsPath).toString('utf8');
-            // for(const line of text.split(os.EOL))
-            // {
-            //     rowCount += Math.ceil(line.length / col);
-            // }
             const stream = fs.createReadStream(fsPath);
             for await(const line of readline.createInterface({input:stream, crlfDelay: Infinity}))
             {
@@ -570,9 +486,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
                 rowCount += Math.ceil(line.length / col);
             }
             pageCount = Math.ceil(rowCount / row);
-            // percentage = pageCount / page;
-            // for(let i=1; i<11; i++){ percentageStr += (percentage * 10 > i)?"#":" "; }
-            // console.log(`length: ${text.length},  page: ${page},  rowCount: ${rowCount},  pageCount: ${pageCount}`);
             console.log(`length: ${textCount},  page: ${page},  rowCount: ${rowCount},  pageCount: ${pageCount}`);
 
             if(max){
@@ -602,7 +515,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         {
             const chars = (matchChars)? Number(matchChars[1]): Number(matchCount[1]);
             let textCount = 0;
-            // const text = fs.readFileSync(fsPath).toString('utf8');
             const stream = fs.createReadStream(fsPath);
             for await(const line of readline.createInterface({input:stream, crlfDelay: Infinity}))
             {
@@ -673,9 +585,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
     /** 読み込み */
     private async getPlot(refresh=false): Promise<void>
     {
-		// ワークスペース内のすべてのルートフォルダ
-        // vscode.workspace.workspaceFolders
-
         this.plot = null;
         
         const plotFile = this.getPlotFile();
@@ -696,51 +605,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         }else{
             throw Error('plotファイルを開けませんでした。');
         }
-
-		// // 現在開いているエディタのフォルダにアクセスする。
-        // const editor = vscode.window.activeTextEditor;
-		// const resource = editor?.document.uri;
-		// if(resource?.scheme === 'file')
-		// {
-		// 	const folder = vscode.workspace.getWorkspaceFolder(resource);
-		// 	console.log('plot-verifier:: activeFolder: ', folder);
-			
-		// 	if(folder){
-		// 		const plotFile = vscode.Uri.joinPath(folder.uri, 'plot.json');
-        //         console.log('plot-verifier:: plot.json:', plotFile);
-        //         // 存在する場合のみ読み込み
-        //         let plot = '{"chapters":[]}';
-        //         if(fs.existsSync(plotFile.fsPath))
-        //         {
-        //             const plotData = await vscode.workspace.fs.readFile(plotFile);
-        //             plot = Buffer.from(plotData).toString('utf8');
-        //         }
-        //         // if(!fs.existsSync(plotFile.fsPath)){
-        //         //     fs.writeFileSync(plotFile.fsPath, Plot.forEmpty());
-        //         //     vscode.window.showInformationMessage("空の定義ファイルを生成しました (0x0)", plotFile.fsPath);
-        //         // }
-                
-		// 		// ファイル読み込み
-        //         // const plotData = await vscode.workspace.fs.readFile(plotFile);
-        //         // const plot = Buffer.from(plotData).toString('utf8');
-                
-        //         // vscode.window.showInformationMessage(plot);
-        //         // this.text = plot;
-        //         // this.tree = json.parseTree(this.text);
-        //         // TODO 切り替え json.parse()
-        //         // JSON処理
-
-        //         // const p:Plot = json.parse(this.text);
-        //         // const p = new Plot(folder, JSON.parse(this.text));
-        //         const p = new Plot(folder, JSON.parse(plot));
-        //         this.plot = p;
-        //         console.log("json.parse(): ", p);
-        //         // ファイル読み込み中に、初回表示が過ぎてしまうため、明示的に呼び出し。
-        //         if(refresh){
-        //             this._onDidChangeTreeData.fire(undefined);
-        //         }
-		// 	}
-        // }
     }
 
     /** 書き込み */
@@ -750,10 +614,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
         const plotFile = this.getPlotFile();
         if(plotFile)
         {
-            // // TODO prot.json のパスを共通化する
-            // const folderUri = vscode.Uri.file(folder);
-            // const plotFile = vscode.Uri.joinPath(folderUri, 'plot.json');
-
             // 書き込み
             await vscode.workspace.fs.writeFile(plotFile, Buffer.from(this.plot.forSerialize()));
         }
@@ -769,23 +629,6 @@ export class PlotVerifyProvider implements vscode.TreeDataProvider<Element>{
      */
     getFolder():string|undefined
     {
-        // // 現在開いているエディタのフォルダ
-        // const editor = vscode.window.activeTextEditor;
-        // const resource = editor?.document.uri;
-        // if(resource?.scheme === 'file')
-        // {
-        //     const folder = vscode.workspace.getWorkspaceFolder(resource);
-        //     if(folder)
-        //     {
-        //         return folder.uri.fsPath;
-        //     }
-        // }
-
-        // // 現在開いているフォルダの先頭
-        // if(vscode.workspace.workspaceFolders?.length > 0)
-        // {
-        //     return vscode.workspace.workspaceFolders[0].uri.fsPath;
-        // }
         return this.getWorkspaceFolder().uri.fsPath;
     }
 
@@ -948,12 +791,6 @@ class Plot
     constructor(root:vscode.WorkspaceFolder, json:object)
     {
         this.root = root;
-
-        // タイトル、概要は未指定の場合、空白文字
-        // this.title = 'title' in json ? json['title']: "";
-        // this.description = 'description' in json ? json['description']: "";
-        
-        // console.log('date: ', d);
         const cs = 'chapters' in json ? json['chapters'] as []: [];
         for(let c of cs)
         {
@@ -1035,23 +872,14 @@ class Chapter
     properties:Element[]=[];
     constructor(json:object)
     {
-        // this.file = 'file' in json ? json['file']: null;
         const file = 'file' in json ? json['file']: null;
-        // this.alert = 'alert' in json ? new Date(json['alert']): null;
-        // this.deadline = 'deadline' in json ? new Date(json['deadline']): null;
-        // this.alert = 'alert' in json ? json['alert']: null;
-        // this.deadline = 'deadline' in json ? json['deadline']: null;
         const deadline = 'deadline' in json ? json['deadline']: undefined;
-        // this.minimum = 'minimum' in json ? json['minimum']: null;
-        // this.maximum = 'maximum' in json ? json['maximum']: null;
         const minimums = 'minimum' in json ? json['minimum'] as []: [];
         const maximums = 'maximum' in json ? json['maximum'] as []: [];
         const conditions = 'conditions' in json ? json['conditions'] as []: [];
 
         this.fileElement = new Element(this, ElementType.file, file, file);
         this.deadlineElement = new Element(this, ElementType.deadline, deadline);
-        // this.minimumElement = new Element(ElementType.MINIMUM, this.minimum);
-        // this.maximumElement = new Element(ElementType.MAXIMUM, this.maximum);
         for(let minimum of minimums)
         {
             this.minimumElements.push(new Element(this, ElementType.minimum, minimum));
@@ -1067,8 +895,6 @@ class Chapter
         
         this.properties.push(this.fileElement);
         this.properties.push(this.deadlineElement);
-        // this.properties.push(this.minimumElement);
-        // this.properties.push(this.maximumElement);
         for(let minimumElement of this.minimumElements)
         {
             this.properties.push(minimumElement);
